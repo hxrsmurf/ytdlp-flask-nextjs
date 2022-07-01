cd6ce6cbd08a1cc95909c8fca3f04aa4733f773e
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-01 17:18:28 -0400

    Add YouTube URL check

--- python.py
+++ python.py
@@ -6,24 +6,29 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def query_records():
     if request.args:
-        video = request.args.get('video')
+        video_url = request.args.get('video')
 
-        def hook(d):
-            if d['status'] == 'finished':
-                return(d['filename'])
+        parse_video_url = video_url.split('/')
 
-        ytdl_opts = {
-            'outtmpl' : 'static/%(uploader)s/%(title)s.%(ext)s',
-            'progress_hooks' : [hook]
-        }
+        if not 'youtube.com' in parse_video_url[2]:
+            return json.dumps({'message': 'Not Youtube'}), 400, {'ContentType':'application/json'}
+        else:
+            def hook(d):
+                if d['status'] == 'finished':
+                    return(d['filename'])
 
-        # http://127.0.0.1:5000/?video=https://www.youtube.com/
+            ytdl_opts = {
+                'outtmpl' : 'static/%(uploader)s/%(title)s.%(ext)s',
+                'progress_hooks' : [hook]
+            }
 
-        with YoutubeDL(ytdl_opts) as ydl:
-            info = ydl.extract_info(video, download=True)
-            filename = ydl.prepare_filename(info)
-            filename = filename.replace('\\','/')
-            return redirect(f'http://127.0.0.1:5000/{filename}')
+            # http://127.0.0.1:5000/?video=https://www.youtube.com/
+
+            with YoutubeDL(ytdl_opts) as ydl:
+                info = ydl.extract_info(video, download=True)
+                filename = ydl.prepare_filename(info)
+                filename = filename.replace('\\','/')
+                return redirect(f'http://127.0.0.1:5000/{filename}')
 
     else:
         with open('data.txt', 'r') as f:
