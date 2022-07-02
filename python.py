eb29e1e7903535c9d0e1e4e4620cec4d09667902
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-02 00:38:18 -0400

    Add daterange option

--- python.py
+++ python.py
@@ -23,11 +23,13 @@ def query_records():
 
             ytdl_opts = {
                 'outtmpl' : 'static/%(uploader)s/%(title)s.%(ext)s',
-                'progress_hooks' : [hook]
+                'progress_hooks' : [hook],
+                'daterange' : 'today-1weeks'
             }
 
             # http://127.0.0.1:5000/?video=https://www.youtube.com/watch?v=KjR0H8N94Ek
             # http://127.0.0.1:5000/?video=https://www.youtube.com/playlist?list=PLIwiAebpd5CJiaj64YaRzbW5XhymIXS6V
+            # http://127.0.0.1:5000/?video=https://www.youtube.com/c/aliensrock
 
             with YoutubeDL(ytdl_opts) as ydl:
                 info = ydl.extract_info(video_url, download=True)
