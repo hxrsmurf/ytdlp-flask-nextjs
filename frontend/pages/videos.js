f04c36192d54e64faddf33f53e19ad26fece2ff1
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-02 16:12:20 -0400

    Add env variable for video url

--- frontend/pages/videos.js
+++ frontend/pages/videos.js
@@ -15,7 +15,7 @@ export default function videos({ results }) {
                             {result.channel}
                             <ul>
                                 {result.videos.map((video,id) => (
-                                    <li key={id}><a href={'http://127.0.0.1:5000/static/' + result.channel + '/' + video}>{video}</a></li>
+                                    <li key={id}><a href={ process.env.API_URL + result.channel + '/' + video}>{video}</a></li>
                                 ))}
                             </ul>
                         </li>
