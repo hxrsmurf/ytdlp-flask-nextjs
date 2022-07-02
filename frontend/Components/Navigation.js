88522c7e95961854f44860a0a648f3df7aa4ab83
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-02 01:21:55 -0400

    Actual navbar links

--- frontend/Components/Navigation.js
+++ frontend/Components/Navigation.js
@@ -7,11 +7,10 @@ export default function Navigation() {
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/videos">Videos</Nav.Link>
+                        <Nav.Link href="/channels">Channels</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
