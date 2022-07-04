905017f9843aa821d1755d650f8c2cc04093b5e3
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-04 10:17:48 -0400

    Redownload channels ps1

---
+++ backend/functions/recreate.ps1
@@ -0,0 +1,23 @@
+$channels = @(
+    "https://www.youtube.com/c/aliensrock",
+    "https://www.youtube.com/c/EntertainTheElk",
+    "https://www.youtube.com/c/Nerdstalgic",
+    "https://www.youtube.com/c/F1",
+    "https://www.youtube.com/c/CasuallyExplained",
+    "https://www.youtube.com/c/Wendoverproductions",
+    "https://www.youtube.com/c/inanutshell",
+    "https://www.youtube.com/user/BostonDynamics",
+    "https://www.youtube.com/c/StuffMadeHere",
+    "https://www.youtube.com/c/MrBeast6000",
+    "https://www.youtube.com/c/SifdGaming",
+    "https://www.youtube.com/c/MattDAvella",
+    "https://www.youtube.com/c/Woolie"
+)
+
+$API_URL = 'http://127.0.0.1:5000/'
+
+forEach ($channel in $channels){
+    $query = $API_URL + 'channels?search=' + $channel
+    Write-Host $query
+    Invoke-WebRequest -Uri $query -METHOD GET
+}
