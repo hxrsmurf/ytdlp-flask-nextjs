6bbb21fdac326016a066837bff3dcc36dedae16b
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-05 18:36:50 -0400

    Delete test files

--- backend/classes/Testing.py
+++
@@ -1,8 +0,0 @@
-from .shared import db
-
-class Testing(db.Model):
-    id = db.Column('id', db.Integer, primary_key = True)
-    testing = db.Column(db.String(100))
-
-    def __init__(self, testing):
-        self.testing = testing
