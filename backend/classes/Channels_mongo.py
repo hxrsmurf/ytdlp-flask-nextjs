e880c48671580b07d7eb5e0ac05dc0063a5fca5b
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-11 19:23:16 -0400

    Basic Channel Add

--- backend/classes/Channels_mongo.py
+++ backend/classes/Channels_mongo.py
@@ -1,8 +1,16 @@
 from .shared import mongo_db
 
-class User(mongo_db.Document):
-    name = mongo_db.StringField()
-    email = mongo_db.StringField()
-
-    def to_json(self):
-        return {'name' : self.name, 'email' : self.email}
+class Channels(mongo_db.Document):
+    channel_id = mongo_db.StringField(primary_key = True)
+    channel_name = mongo_db.StringField()
+    channel_name_lowercase = mongo_db.StringField()
+    channel_follower_count = mongo_db.IntField()
+    description = mongo_db.StringField()
+    original_url = mongo_db.StringField()
+    uploader = mongo_db.StringField()
+    uploader_id = mongo_db.StringField()
+    webpage_url = mongo_db.StringField()
+    picture_profile = mongo_db.StringField()
+    picture_cover = mongo_db.StringField()
+    last_updated = mongo_db.StringField()
+    latest_upload = mongo_db.StringField()
