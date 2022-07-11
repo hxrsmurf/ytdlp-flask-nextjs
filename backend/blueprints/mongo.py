71275403eda158487586dc83ab3e7d363e054bb9
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-11 19:36:40 -0400

    Add channels add/get

--- backend/blueprints/mongo.py
+++ backend/blueprints/mongo.py
@@ -1,18 +1,41 @@
-from flask import Blueprint, jsonify
+import json
+from flask import Blueprint, jsonify, request
 
-from classes.shared import mongo_db
+from functions.downloader import download
+from functions.utils import getCurrentTime
 from classes import Channels_mongo
 
 mongo_bp = Blueprint('mongo', __name__, url_prefix='/mongo')
 
-@mongo_bp.route('/', methods=['GET'])
+@mongo_bp.route('/channels/', methods=['GET'])
 def root():
-    user = Channels_mongo.User.objects()
-    #user.save()
-    t = []
-    for u in user:
-        t.append({
-            'name' : u['name']
-        })
+    query = Channels_mongo.Channels.objects()
+    query_json = json.loads(query.to_json())
+    return(jsonify(query_json))
 
-    return(jsonify(t))
+@mongo_bp.route('/channels/add', methods=['GET'])
+def add():
+    url = request.args['url']
+    download_result = download(video=url, video_range=1, download_confirm=False)
+
+    channel_name_lowercase = download_result['channel'].lower()
+
+    query = Channels_mongo.Channels(
+        channel_id = download_result['channel_id'],
+        channel_name = download_result['channel'],
+        channel_name_lowercase = channel_name_lowercase,
+        channel_follower_count = download_result['channel_follower_count'],
+        description = download_result['description'],
+        original_url = download_result['original_url'],
+        uploader = download_result['uploader'],
+        uploader_id = download_result['uploader_id'],
+        webpage_url = download_result['webpage_url'],
+        picture_profile = download_result['thumbnails'][18]['url'],
+        picture_cover = download_result['thumbnails'][15]['url'],
+        last_updated = getCurrentTime(),
+        latest_upload = download_result['entries'][0]['original_url']
+    )
+
+    query.save()
+
+    return(json.loads(query.to_json()))
