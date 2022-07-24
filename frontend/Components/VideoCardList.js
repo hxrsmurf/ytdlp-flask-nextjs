c554e7c1429607c47bf621532d518a37cab3972d
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-24 11:00:45 -0400

    Add NextJS Image to VideoCardList

--- frontend/Components/VideoCardList.js
+++ frontend/Components/VideoCardList.js
@@ -6,6 +6,8 @@ import Card from 'react-bootstrap/Card'
 
 import VideoButtons from './VideoButtons'
 
+import Image from 'next/image'
+
 export default function VideoCardList({ data }) {
 
     const handleClick = async (event) => {
@@ -18,11 +20,22 @@ export default function VideoCardList({ data }) {
                 {data.map((result, id) => (
                     <Col lg='4' key={id} style={{ width: '260px', height: '400px' }} className='mt-5'>
                         <Card key={id}>
-                            <Card.Img
-                                variant="top" src={result.thumbnail}
-                                onClick={(e) => handleClick(result.original_url)}
-                                style={{ cursor: "pointer", height: '150px', width: '100%' }}
-                            />
+                            <Card.Body>
+                                {result.thumbnail ?
+                                    <Image
+                                        src={result.thumbnail}
+                                        width={1200}
+                                        height={800}
+                                        layout='responsive'
+                                        lazyBoundary='1px'
+                                        quality={100}
+                                        style={{ cursor: "pointer"}}
+                                        onClick={(e) => handleClick(result.original_url)}
+                                        />
+                                :
+                                    <></>
+                                }
+                            </Card.Body>
                             <Card.Header style={{ height: '240px', fontSize: '.9rem' }}>
                                 <Row>
                                     <Row><div style={{ height: '40px', overflow: 'hidden', textOverflow: 'ellipsis' }}>{result.title}</div></Row>
