042b8050599a2cd058fa5358c9e40962f3ccb694
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-24 11:05:26 -0400

    Feature - VideoCard Thumbnail - Use NextJS Image Optimization (#35)
    * Add NextJS Image to VideoCardList
    * Add ytimg to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['cdn']}
+  images: { domains: ['cdn', 'i.ytimg.com']}
 }
 
 module.exports = nextConfig
