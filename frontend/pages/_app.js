09d394d944b83f89814e4b9c49c165eb547d6059
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-24 19:31:44 -0400

    import fontsource roboto

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "../Components/Navigation"
 import 'video.js/dist/video-js.min.css'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
