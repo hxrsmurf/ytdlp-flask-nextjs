d586e44f0b8a77c106362deaa685f953adf3bca9
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-24 20:29:19 -0400

    Remove extranous imports

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,12 +11,6 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import { useState } from 'react';
 import Sidebar from '../Components/Material/Menu/Sidebar';
