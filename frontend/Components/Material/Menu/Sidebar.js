b980ff7846f4eadece8a2d157ba3b9d7131f361f
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-25 20:48:27 -0400

    Increase icon size for drawer

--- frontend/Components/Material/Menu/Sidebar.js
+++ frontend/Components/Material/Menu/Sidebar.js
@@ -7,12 +7,12 @@ import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
 export default function Sidebar({open, title, icon, url}) {
     return (
         <>
-            <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItem disablePadding sx={{ display: 'block',  mt:2 }}>
                 <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5 }} href={url}>
                     <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center' }}>
-                        {icon == 'Home' ? <HomeIcon/> : <></>}
-                        {icon == 'Subscriptions' ? <SubscriptionsIcon/> : <></>}
-                        {icon == 'Library' ? <VideoLibraryIcon/> : <></>}
+                        {icon == 'Home' ? <HomeIcon sx={{fontSize: 35}}/> : <></>}
+                        {icon == 'Subscriptions' ? <SubscriptionsIcon sx={{fontSize: 35}}/> : <></>}
+                        {icon == 'Library' ? <VideoLibraryIcon sx={{fontSize: 35}}/> : <></>}
                     </ListItemIcon>
                     <ListItemText primary={title} sx={{ opacity: open ? 1 : 0 }}/>
                 </ListItemButton>
