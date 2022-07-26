00980a3e89f389183ab100d2e54795a735ce4956
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-26 07:56:21 -0400

    Convert MissingChannels to Material

--- frontend/Components/MissingChannels.js
+++ frontend/Components/MissingChannels.js
@@ -1,19 +1,26 @@
-import Container from 'react-bootstrap/Container'
+import { Box, List, ListItem, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
+
 export default function MissingChannels({ data }) {
     return (
         <>
-            <Container className='mt-5'>
-                Manually add these channels
-                <ul>
+            <Box className='mt-5' sx={{ border: true, width: '700px', overflow: 'hidden', whiteSpace: 'nowrap' }}>
+                <List
+                    className='mt-2'
+                    subheader={<ListSubheader>Manually add these channels</ListSubheader>}
+                >
                     {data.map((channel, id) => (
                         <>
-                            <li key={id}>
-                                <a href={channel.original_url} rel='noopener'>{channel.channel_name} - {channel.title}</a>
-                            </li>
+                            <ListItem key={id} disablePadding>
+                                <ListItemButton component="a" href={channel.original_url}>
+                                    <ListItemText>
+                                        {channel.channel_name} - {channel.title}
+                                    </ListItemText>
+                                </ListItemButton>
+                            </ListItem>
                         </>
                     ))}
-                </ul>
-            </Container>
+                </List>
+            </Box>
         </>
     )
 }
