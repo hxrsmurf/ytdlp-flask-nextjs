d015d67228ac2ac1992003920b75203b71e7386f
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-26 21:04:08 -0400

    Update VideoCardList to material

--- frontend/Components/VideoCardList.js
+++ frontend/Components/VideoCardList.js
@@ -1,12 +1,13 @@
-import { HandThumbsUp, Dot } from 'react-bootstrap-icons'
-
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-
 import VideoButtons from './VideoButtons'
 
-import Image from 'next/image'
+import { CardActionArea, Grid } from '@mui/material'
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
 
 export default function VideoCardList({ data }) {
 
@@ -16,41 +17,36 @@ export default function VideoCardList({ data }) {
 
     return (
         <>
-            <Row style={{ height: '150px' }}>
+            <Grid container spacing={4}>
                 {data.map((result, id) => (
-                    <Col lg='4' key={id} style={{ width: '290px', height: '400px' }} className='mt-5'>
-                        <Card key={id}>
-                            <Card.Body>
-                                {result.thumbnail ?
-                                    <Image
-                                        src={result.thumbnail}
-                                        width={1200}
-                                        height={800}
-                                        layout='responsive'
-                                        lazyBoundary='1px'
-                                        quality={100}
-                                        style={{ cursor: "pointer"}}
-                                        onClick={(e) => handleClick(result.original_url)}
-                                        />
-                                :
-                                    <></>
-                                }
-                            </Card.Body>
-                            <Card.Header style={{ height: '240px', fontSize: '.9rem' }}>
-                                <Row>
-                                    <Row><div style={{ height: '40px', overflow: 'hidden', textOverflow: 'ellipsis' }}>{result.title}</div></Row>
-                                    <Row className='mt-3'><div>{result.channel_name}</div></Row>
-                                    <Row className='mt-3'><div>{result.view_count} Views<Dot />{result.upload_date}</div></Row>
-                                    <Row className='mt-3'><div>{result.duration_string} minutes<Dot /><HandThumbsUp /> {result.like_count}</div></Row>
-                                    <Row>
-                                        <VideoButtons data={result}/>
-                                    </Row>
-                                </Row>
-                            </Card.Header>
+                    <Grid item spacing={4}>
+                        <Card sx={{ width: 255, height: 320, backgroundColor: '#edebea' }}>
+                            <CardActionArea>
+                                <CardHeader
+                                    title={<Typography noWrap>{result.title}</Typography>}
+                                    subheader={result.channel_name}
+                                    sx={{ display: 'block', overflow: 'hidden' }}
+                                />
+                                <CardMedia
+                                    component='img'
+                                    height='140'
+                                    image={result.thumbnail}
+                                />
+                                <CardContent>
+                                    <Grid container spacing={1} justifyContent='center' alignItems='center'>
+                                        <Grid item>{result.view_count} views</Grid>
+                                        <Grid item><span className='dot'></span></Grid>
+                                        <Grid item>{result.upload_date}</Grid>
+                                    </Grid>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions sx={{ height: 2 }}>
+                                <VideoButtons data={result} />
+                            </CardActions>
                         </Card>
-                    </Col>
+                    </Grid>
                 ))}
-            </Row>
+            </Grid>
         </>
     )
 }
