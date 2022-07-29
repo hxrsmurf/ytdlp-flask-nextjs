9b84f51ccbff85a2f9d49d5b685c828bb1abc91b
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-29 18:07:27 -0400

    Initial bunnycdn function

---
+++ backend/functions/bunnycdn_upload.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv('../.env')
+
+library = os.environ.get("BUNNYCDN_LIBRARY")
+api_key = os.environ.get("BUNNYCDN_KEY")
+base_url = f'https://video.bunnycdn.com/library/{library}/videos'
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/*+json",
+    "AccessKey": api_key
+}
+
+def bunnycdn_create_video(title):
+    print('Getting Video GUID')
+    payload = json.dumps({
+        'title': title
+    })
+
+    response = requests.post(base_url, data=payload, headers=headers)
+    return json.loads(response.text)['guid']
+
+def bunnycdn_upload(title, video_id, url):
+    guid = bunnycdn_create_video(title)
+    print(f'Got {guid}')
+    format_video_file = f'{video_id}/{video_id}.mp4'
+
+    upload_video_file = open(format_video_file,'rb')
+    video_url = f'{base_url}/{guid}'
+    print('Uploading to BunnyCDN')
+    response = requests.put(video_url, headers=headers, data=upload_video_file)
+    if response.status_code == 200:
+        print('Completed Upload to BunnyCDN')
+    else:
+        print('Failure to upload')
