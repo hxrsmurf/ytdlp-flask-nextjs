24f4167eac51bf453595807fde25f7a3b1ef7974
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-30 12:01:37 -0400

    Add restricted_ips

--- aws/lambda-ec2/python/index.py
+++ aws/lambda-ec2/python/index.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import functions.convert_user_data as convert_user_data
 import functions.run_ec2 as run_ec2
@@ -9,6 +10,12 @@ def handler(event, context):
     json_event = json.loads(json.dumps(event))
     request_context = json_event['requestContext']
     source_ip = request_context['http']['sourceIp']
+
+    restricted_ips = os.environ['RESTRICTED_IPS']
+
+    if not source_ip in restricted_ips:
+        return({'statusCode': 401, 'body': 'Not Authorized'})
+
     try:
         requested_video_id = json_event['queryStringParameters']['id']
     except:
