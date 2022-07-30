a7281e520857e3d69b7a6b7cac2cb1b988c1d778
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-30 09:56:24 -0400

    Start source IP check from API Gateway

--- digitalocean/aws/lambda-ec2/python/index.py
+++ digitalocean/aws/lambda-ec2/python/index.py
@@ -1,7 +1,13 @@
-import boto3
+import json
+
 import functions.convert_user_data as convert_user_data
 import functions.run_ec2 as run_ec2
+
 def handler(event, context):
+    json_event = json.loads(json.dumps(event))
+    request_context = json_event['requestContext']
+    source_ip = request_context['http']['sourceIp']
+
     # sam build; sam deploy --no-confirm-changeset
 
     base64UserData = convert_user_data.convert(video_id='RlOB3UALvrQ')
