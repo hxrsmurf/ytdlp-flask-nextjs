7d26feb6066650814756dc9e7e4e2723834dcb6e
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-30 09:15:55 -0400

    Basic boto3 run spot instance

--- digitalocean/aws/lambda-ec2/python/index.py
+++ digitalocean/aws/lambda-ec2/python/index.py
@@ -2,7 +2,29 @@ import boto3
 
 def handler(event, context):
     # sam build; sam deploy --no-confirm-changeset
-    print(event)
+    client = boto3.client('ec2')
+
+    response = client.run_instances(
+        ImageId='ami-09a41e26df464c548',
+        InstanceType='t2.micro',
+        KeyName='nvme-virginia',
+        MinCount=1,
+        MaxCount=1,
+        NetworkInterfaces=[{
+            'AssociatePublicIpAddress': True,
+            'DeleteOnTermination': True,
+            'DeviceIndex': 0,
+            'SubnetId': 'subnet-0d8fa1295c21f9ead'
+        }],
+        InstanceMarketOptions={
+            'MarketType': 'spot',
+            'SpotOptions': {
+                'SpotInstanceType' : 'one-time'
+            }
+        }
+    )
+
+    print(response)
     return({
             'statusCode': 200,
             'body': 'Success'
