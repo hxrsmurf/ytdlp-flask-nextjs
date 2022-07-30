4191fb77c32623b879c90c344725b858bfc79516
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-30 11:41:39 -0400

    move/create aws directory top-level

---
+++ aws/lambda-ec2/python/index.py
@@ -0,0 +1,23 @@
+import json
+
+import functions.convert_user_data as convert_user_data
+import functions.run_ec2 as run_ec2
+
+# sam build; sam deploy --no-confirm-changeset
+
+def handler(event, context):
+    json_event = json.loads(json.dumps(event))
+    request_context = json_event['requestContext']
+    source_ip = request_context['http']['sourceIp']
+    try:
+        requested_video_id = json_event['queryStringParameters']['id']
+    except:
+        return({'statusCode': 200, 'body': 'Not current query string'})
+
+    user_data = convert_user_data.convert(video_id=requested_video_id)
+    run_ec2.run_instance(user_data=user_data, instance_type='t3.nano')
+
+    return({
+            'statusCode': 200,
+            'body': 'Success'
+        })
