64f3df35a46863eab964a6653351c637d6d5d17d
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-30 09:56:13 -0400

    remove extra spacing

--- digitalocean/aws/lambda-ec2/python/functions/convert_user_data.py
+++ digitalocean/aws/lambda-ec2/python/functions/convert_user_data.py
@@ -1,4 +1,5 @@
 import base64
+import re
 
 def convert(video_id):
     data = f'\
@@ -10,7 +11,7 @@ def convert(video_id):
     cd /root/\n\
     yt-dlp --format bestvideo*+bestaudio/best --merge-output-format mp4 --output \'%(id)s/%(id)s.%(ext)s\' https://youtu.be/{video_id}\
     '
-    return(data)
+    return(re.sub(r"    ", "", data))
 
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.run_instances
     # This value will be base64 encoded automatically. Do not base64 encode this value prior to performing the operation.
@@ -18,3 +19,4 @@ def convert(video_id):
     encodedBytes = base64.b64encode(data.encode("utf-8"))
     encodedStr = str(encodedBytes, "utf-8")
     print(encodedStr)
+print(convert('video_id'))
