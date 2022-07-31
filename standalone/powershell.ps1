81245c515a8b51bd34130678fd0e5567ca86adba
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-31 04:27:09 -0400

    Add hevc_nvenc to standalone powershell.ps1

--- standalone/powershell.ps1
+++ standalone/powershell.ps1
@@ -3,3 +3,6 @@ $directory = ''
 
 docker run -v ${directory}:/media:Z tnk4on/yt-dlp --format bestvideo*+bestaudio/best --merge-output-format mp4 --output '%(id)s/%(id)s.%(ext)s' https://youtu.be/$video_id
 ffmpeg -i $video_id/$video_id.mp4 -c:v libx264 -c:a copy -flags +cgop -g 30 -hls_time 1 -hls_playlist_type event $video_id/$video_id.m3u8
+
+# https://github.com/BtbN/FFmpeg-Builds/releases
+ffmpeg -i $video_id/$video_id.mp4 -c:v hevc_nvenc -c:a copy -flags +cgop -g 30 -hls_time 1 -hls_playlist_type event $video_id/$video_id.m3u8
