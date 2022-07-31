3699aa522755a3c384651fb78d48f7ec71d2b98b
Kevin Murphy <kevin@hxrsmurf.info>
2022-07-31 06:29:05 -0400

    import timedelta and update functions

--- backend/functions/utils.py
+++ backend/functions/utils.py
@@ -1,5 +1,9 @@
-import datetime
+from datetime import datetime, timedelta
 
 def getCurrentTime():
-    now = datetime.datetime.now()
+    now = datetime.now()
     return(now.strftime('%Y-%m-%d %H:%M:%S:%f'))
+
+def getCurrentDateString():
+    now = datetime.now() - timedelta(2)
+    return(now.strftime('%Y%m%d'))
