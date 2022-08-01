2aff647875fbe841b5eec69043908b466fee5675
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-01 19:20:04 -0400

    Remove doule MongoDB call redis_publish

--- backend/functions/utils.py
+++ backend/functions/utils.py
@@ -13,11 +13,7 @@ def getInitialVideosToLoad():
     now = datetime.now() - timedelta(int(os.environ.get('COUNT_VIDEOS_TO_LOAD')))
     return(now.strftime('%Y%m%d'))
 
-def redis_publish(video_id,url):
-    query = json.loads((Mongo.Videos.objects(video_id=video_id)).to_json())[0]
-    video_id = query['_id']
-    original_url = query['original_url']
-
+def redis_publish(video_id, video_url):
     redis_db = redis.Redis(
         host='127.0.0.1',
         port='6379',
@@ -25,6 +21,6 @@ def redis_publish(video_id,url):
     )
 
     redis_channel='download_queue'
-    message = str({'url': url, 'video_id': video_id})
+    message = str({'url': video_url, 'video_id': video_id})
     redis_db.publish(redis_channel, message)
     return(message)
