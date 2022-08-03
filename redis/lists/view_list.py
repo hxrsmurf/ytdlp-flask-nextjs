61cd2aa4af51e3542c287bacae63aed18de15154
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-02 20:39:50 -0400

    Update view_list.py

--- redis/lists/view_list.py
+++ redis/lists/view_list.py
@@ -2,8 +2,8 @@ import time
 
 from shared import redis_db
 
-redis_queue_name = 'download_queue'
+redis_queue_name = 'download-queue'
 
 while True:
     print(redis_db().lrange(redis_queue_name, 0, -1))
-    time.sleep(10)
+    time.sleep(2)
