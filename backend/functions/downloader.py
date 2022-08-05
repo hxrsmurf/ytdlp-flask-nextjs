bf60b6a73839ffc7863bf0845f3db03c1f59c17b
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-04 21:26:28 -0400

    Update Mongo after uploading thumbnail

--- backend/functions/downloader.py
+++ backend/functions/downloader.py
@@ -1,5 +1,8 @@
+import os
 import requests
 from yt_dlp import YoutubeDL
+
+from classes import Mongo
 
 from .backblaze_upload import b2_upload
 
@@ -28,9 +31,12 @@ def download(video, video_range=2, download_confirm=False):
 def download_thumbnail(thumbnail_url, video_id):
     video_thumbnail_output_name = f'{video_id}.jpg'
     video_thumbnail_folder = 'video_thumbnails'
+    cdn_video_thumbnail_url = f'{os.environ.get("CDN_URL")}/{os.environ.get("B2_BUCKET")}/{video_thumbnail_folder}/{video_thumbnail_output_name}'
+
     if '8ZMxwS57b4A' in thumbnail_url:
         thumbnail_request = requests.get(thumbnail_url, stream=True)
         if thumbnail_request.status_code == 200:
             with open(video_thumbnail_output_name, 'wb') as file:
                 file.write(thumbnail_request.content)
             b2_upload(file=video_thumbnail_output_name,folder=video_thumbnail_folder)
+        Mongo.Videos.objects(video_id=video_id).update_one(set__cdn_video_thumbnail=cdn_video_thumbnail_url)
