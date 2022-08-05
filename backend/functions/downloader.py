6ad53c37a067bbe0a8e6bcc6a51a726505461fd4
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-04 21:29:41 -0400

    If on CDN check

--- backend/functions/downloader.py
+++ backend/functions/downloader.py
@@ -33,10 +33,18 @@ def download_thumbnail(thumbnail_url, video_id):
     video_thumbnail_folder = 'video_thumbnails'
     cdn_video_thumbnail_url = f'{os.environ.get("CDN_URL")}/{os.environ.get("B2_BUCKET")}/{video_thumbnail_folder}/{video_thumbnail_output_name}'
 
-    if '8ZMxwS57b4A' in thumbnail_url:
+    check_cdn = requests.get(cdn_video_thumbnail_url)
+    if check_cdn.status_code == 200:
+        return(cdn_video_thumbnail_url)
+    else:
         thumbnail_request = requests.get(thumbnail_url, stream=True)
         if thumbnail_request.status_code == 200:
             with open(video_thumbnail_output_name, 'wb') as file:
                 file.write(thumbnail_request.content)
             b2_upload(file=video_thumbnail_output_name,folder=video_thumbnail_folder)
-        Mongo.Videos.objects(video_id=video_id).update_one(set__cdn_video_thumbnail=cdn_video_thumbnail_url)
+
+            Mongo.Videos.objects(video_id=video_id).update_one(set__cdn_video_thumbnail=cdn_video_thumbnail_url)
+
+            if os.path.exists(video_thumbnail_output_name):
+                os.remove(video_thumbnail_output_name)
+            return(cdn_video_thumbnail_url)
