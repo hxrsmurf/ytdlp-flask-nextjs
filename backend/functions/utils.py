5795f7806a3aaa01dfa636744e2e6a967cf29f60
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-06 18:13:08 -0400

    Hotfix - Fix Improper Redis Caching for Videos and Channels (#133)
    * Update video and channels cache to detect if cached better
    * Add REDIS_CACHE_LENGTH to env

--- backend/functions/utils.py
+++ backend/functions/utils.py
@@ -44,22 +44,23 @@ def redis_add_to_list(video_id, video_url, duration):
 
 
 def redis_cache_videos(data=None):
-    if not redis_db.get('videos'):
+    cached_videos = redis_db.get('videos')
+    if cached_videos == 'None' or cached_videos == None:
         redis_db.set('videos', str(data))
-        redis_db.expire('videos',600)
-        return None
+        redis_db.expire('videos',os.environ.get('REDIS_CACHE_LENGTH'))
+        cached_videos = redis_db.get('videos')
+        return(ast.literal_eval(cached_videos))
     else:
-        result = redis_db.get('videos')
-        return(ast.literal_eval(result))
+        return(ast.literal_eval(cached_videos))
 
 def redis_cache_channels(data=None):
     cached_channels = redis_db.get('channels')
-    if cached_channels:
+    if cached_channels == 'None' or cached_channels == None:
+        redis_db.set('channels', str(data))
+        redis_db.expire('channels', os.environ.get('REDIS_CACHE_LENGTH'))
+        cached_channels = redis_db.get('channels')
         return(ast.literal_eval(cached_channels))
     else:
-        redis_db.set('channels', str(data))
-        redis_db.expire('channels', 600)
-        cached_channels = redis_db.get('channels')
         return(ast.literal_eval(cached_channels))
 
 def redis_cache_channels_unique(data=None):
@@ -67,7 +68,7 @@ def redis_cache_channels_unique(data=None):
     if cached_channels_unique == 'None' or cached_channels_unique == None:
         print("go cache")
         redis_db.set('channels_unique', str(data))
-        redis_db.expire('channels_unique', 5)
+        redis_db.expire('channels_unique', os.environ.get('REDIS_CACHE_LENGTH'))
         cached_channels_unique = redis_db.get('channels_unique')
         return(ast.literal_eval(cached_channels_unique))
     else:
