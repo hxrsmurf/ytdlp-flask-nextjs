f7aba08d0734d4991821e59155d71ab8584e7e6d
Kevin Murphy <kevin@hxrsmurf.info>
2022-08-06 18:11:52 -0400

    Add REDIS_CACHE_LENGTH to env

--- backend/functions/utils.py
+++ backend/functions/utils.py
@@ -47,7 +47,7 @@ def redis_cache_videos(data=None):
     cached_videos = redis_db.get('videos')
     if cached_videos == 'None' or cached_videos == None:
         redis_db.set('videos', str(data))
-        redis_db.expire('videos',600)
+        redis_db.expire('videos',os.environ.get('REDIS_CACHE_LENGTH'))
         cached_videos = redis_db.get('videos')
         return(ast.literal_eval(cached_videos))
     else:
@@ -57,7 +57,7 @@ def redis_cache_channels(data=None):
     cached_channels = redis_db.get('channels')
     if cached_channels == 'None' or cached_channels == None:
         redis_db.set('channels', str(data))
-        redis_db.expire('channels', 600)
+        redis_db.expire('channels', os.environ.get('REDIS_CACHE_LENGTH'))
         cached_channels = redis_db.get('channels')
         return(ast.literal_eval(cached_channels))
     else:
@@ -68,7 +68,7 @@ def redis_cache_channels_unique(data=None):
     if cached_channels_unique == 'None' or cached_channels_unique == None:
         print("go cache")
         redis_db.set('channels_unique', str(data))
-        redis_db.expire('channels_unique', 5)
+        redis_db.expire('channels_unique', os.environ.get('REDIS_CACHE_LENGTH'))
         cached_channels_unique = redis_db.get('channels_unique')
         return(ast.literal_eval(cached_channels_unique))
     else:
