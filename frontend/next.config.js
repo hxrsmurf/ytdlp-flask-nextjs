7464ef48d3e4bdd7f8a372b9b058d83c8a25f3c2
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-31 15:06:51 -0500

    Delete old frontend

--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { domains: ['cdn-youtube.kvchmurphy.com', 'i.ytimg.com']}
-}
-
-module.exports = nextConfig
