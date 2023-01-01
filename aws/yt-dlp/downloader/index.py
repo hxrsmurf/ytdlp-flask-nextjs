6d29b304f2775133f9fe020e69418b281e6f9989
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 18:26:30 -0500

    subprocess ffmpeg

--- aws/yt-dlp/downloader/index.py
+++ aws/yt-dlp/downloader/index.py
@@ -1,6 +1,6 @@
-import json
 import logging
 import os
+import subprocess
 
 from functions.download import download
 
@@ -9,12 +9,14 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     URL = 'https://www.youtube.com/@MrBeast'
-    video_id = '7IKab3HcfFk'
+    #video_id = '7IKab3HcfFk'
     video_url = 'https://www.youtube.com/watch?v=7IKab3HcfFk'
-    download(video_url)
-
-    complete = os.listdir('/tmp/')
-    print(complete)
+    video_id = download(video_url)
+    videos = os.listdir('/tmp/')
+    for video in videos:
+        ffmpeg_cmd = f'/opt/python/ffmpeg -i /tmp/{video} -c copy -flags +cgop -g 30 -hls_time 2 -hls_playlist_type event /tmp/{video_id}.m3u8'
+        subprocess.run(ffmpeg_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(os.listdir('/tmp/'))
 
     return({
             'statusCode': 200,
