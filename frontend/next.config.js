15bb45bff88b823dce482c50f8d90ae999218025
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-31 23:31:51 -0500

    Update subscriptions to show channel cover photo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.googleusercontent.com',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+        pathname: '**'
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- frontend/pages/subscriptions.tsx
+++ frontend/pages/subscriptions.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { Key } from 'react'
 import { redis } from '../lib/database'
@@ -6,11 +7,12 @@ export default function subscriptions({ channels }: any) {
   return (
     <div className='flex justify-center mt-4'>
       <div>
-        {channels.map((channel: String, id: Key) => (
-          <div key={id} className='grid'>
-            <div>
-              <Link href={String(channel)} passHref legacyBehavior>
-                <a target='_blank'>{channel}</a>
+        {channels.map((channel: any, id: Key) => (
+          <div key={id} className='mt-4'>
+            <div className='hover:cursor-pointer'>
+              {channel.url}
+              <Link href={String(channel.url)} passHref legacyBehavior>
+                <Image src={channel.cover[0]} alt='' width={1000} height={1000} />
               </Link>
             </div>
           </div>
