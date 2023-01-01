b28cac52d16bb7293f0b2545d12cec76a7a1fd62
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 13:56:12 -0500

    Add testing feature flag

--- aws/yt-dlp/python/index.py
+++ aws/yt-dlp/python/index.py
@@ -2,19 +2,24 @@ import json
 import logging
 
 from functions.download import get_channel
+from functions.database import get_item
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    json_event = json.loads(json.dumps(event))
-    request_context = json_event['requestContext']
-    query_string = json_event['queryStringParameters']
-    logging.info(query_string)
-
-    URL = 'https://www.youtube.com/@MrBeast'
-
-    channel_info = get_channel(query_string['id'])
+    testing = True
+    if not testing:
+        json_event = json.loads(json.dumps(event))
+        request_context = json_event['requestContext']
+        query_string = json_event['queryStringParameters']
+        channel_info = get_channel(query_string['id'])
+    else:
+        URL = 'https://www.youtube.com/@MrBeast'
+        video_id = '7IKab3HcfFk'
+        get_item(video_id)
+        #channel_info = get_channel(URL)
+        channel_info = 'kevin'
 
     return({
             'statusCode': 200,
