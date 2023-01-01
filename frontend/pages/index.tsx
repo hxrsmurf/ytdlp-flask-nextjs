2edd34b94e4a192a3742d36b05c26d34ed80d6da
Kevin Murphy <kevin@hxrsmurf.info>
2022-12-31 22:26:36 -0500

    Placeholder main video page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,13 +3,24 @@ import { redis } from '../lib/database'
 
 export default function Home() {
   return (
-    <div className='flex justify-center mt-4'>
-      <div>
+    <>
+      <div className='flex justify-center mt-4'>
         <div>
-          <AddChannel />
+          <div>
+            <AddChannel />
+          </div>
+          <div>
+            <div className='grid grid-cols-5'>
+              {list_videos.map((video, id) => (
+                <div key={id} className='mt-14'>
+                  <div>{video.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
@@ -24,3 +35,24 @@ export async function getServerSideProps() {
     },
   }
 }
+
+const list_videos = [
+  {
+    name: 'Test',
+  },
+  {
+    name: 'Test',
+  },
+  {
+    name: 'Test',
+  },
+  {
+    name: 'Test',
+  },
+  {
+    name: 'Test',
+  },
+  {
+    name: 'Test',
+  },
+]
