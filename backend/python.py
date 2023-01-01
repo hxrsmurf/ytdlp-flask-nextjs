a452be55b02be3aa61f594682ba4deee13342ceb
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 18:45:30 -0500

    Add basic download command to backend flask

--- backend/python.py
+++ backend/python.py
@@ -37,5 +37,13 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def default_route():
     return('Default')
 
+@app.route('/download', methods=['GET'])
+def download_web():
+    url = request.args['url']
+    range = 1
+    confirm = False
+    download_result = download(video=url, video_range=range, download_confirm=confirm)
+    return download_result
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
