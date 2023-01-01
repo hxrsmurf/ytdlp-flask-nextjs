a232b5babe571f89e9d148635a1699769cc0a1e7
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 08:43:23 -0500

    Add yt-dlp to main function

--- aws/yt-dlp/python/index.py
+++ aws/yt-dlp/python/index.py
@@ -1,17 +1,27 @@
 import json
 import os
 import logging
+import yt_dlp
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
     json_event = json.loads(json.dumps(event))
-    request_context = json_event['requestContext']
+    #request_context = json_event['requestContext']
+    #logging.info(request_context)
 
-    logging.info(request_context)
+    URL = 'https://www.youtube.com/@MrBeast'
+
+    ydl_opts = {
+        'playlistend': 1
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(URL, download=False)
+        info = json.dumps(ydl.sanitize_info(info))
 
     return({
             'statusCode': 200,
-            'body': 'Success'
+            'body': info
         })
