8608d23c2c447ace8e80667e2482070a9419dc2c
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-02 17:58:10 -0500

    refactor utils

--- aws/yt-dlp/parse-url/functions/utils.py
+++ aws/yt-dlp/parse-url/functions/utils.py
@@ -3,22 +3,28 @@ import yt_dlp
 import logging
 
 from .parse import parse_info
-from .database import get_item, put_item
+from .database import get_item, put_item_channel
 
 def download(url):
     ydl_opts = {
-        'playlistend': 1
+        'playlistend': 1,
+        'quiet': True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         info = json.dumps(ydl.sanitize_info(info))
 
-    info, id, type = parse_info(info)
+    return parse_info(info)
 
-    return info, id, type
-
-def check_database(info, id, type):
+def check_database(url_info):
     logging.info('Checking database')
-    # in_database = get_item(id)
-    put_item(id, info)
+
+    original_url = ''.join(url_info['original_url'])
+    latest_video_original_url = ''.join(url_info['latest_video_original_url'])
+    latest_video_original_url_database = get_item(original_url)
+
+    if not latest_video_original_url == latest_video_original_url_database:
+        put_item_channel(original_url, url_info)
+    else:
+        logging.info('Already in database')
