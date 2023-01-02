c027d8d0296b0b1c4160608461e9c0c4fd104bfd
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 20:46:43 -0500

    Update downloader index.py

--- aws/yt-dlp/downloader/index.py
+++ aws/yt-dlp/downloader/index.py
@@ -1,22 +1,17 @@
 import logging
+import requests
 import os
-import subprocess
-
-from functions.download import download
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
     URL = 'https://www.youtube.com/@MrBeast'
-    #video_id = '7IKab3HcfFk'
+    video_id = '7IKab3HcfFk'
     video_url = 'https://www.youtube.com/watch?v=7IKab3HcfFk'
-    video_id = download(video_url)
-    videos = os.listdir('/tmp/')
-    for video in videos:
-        ffmpeg_cmd = f'/opt/python/ffmpeg -i /tmp/{video} -c copy -flags +cgop -g 30 -hls_time 2 -hls_playlist_type event /tmp/{video_id}.m3u8'
-        subprocess.run(ffmpeg_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(os.listdir('/tmp/'))
+    base_url = os.environ['ApiUrl']
+    url = f'{base_url}/download?url={video_url}'
+    requests.get(url)
 
     return({
             'statusCode': 200,
