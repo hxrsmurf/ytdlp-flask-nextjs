6bfe50d24d7d3fd11922e117e6bfac8d5ce0e1c1
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-01 21:01:26 -0500

    Create Downloader with Lambda and Python Flask (Dockerized) (#142)
    * Initial download function
    
    * return video id
    
    * subprocess ffmpeg
    
    * Add basic download command to backend flask
    
    * Add separate dockerfile
    
    * Update docker file for linux
    
    * Update downloader index.py
    
    * Update SQS parser
    
    * Create requests layer folder
    
    * Update parameter

---
+++ aws/yt-dlp/app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from functions.downloader import download
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return "Hello World!"
+
+@app.route('/download', methods=['GET'])
+def download_web():
+    url = request.args['url']
+    range = 1
+    confirm = True
+    download_result = download(video=url, video_range=range, download_confirm=confirm)
+    return ('Success')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
--- aws/yt-dlp/sqs-parser/index.py
+++ aws/yt-dlp/sqs-parser/index.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+import os
 
 from functions.download import get_channel
 from functions.database import get_item, put_item
@@ -8,10 +10,13 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     url = event['Records'][0]['body']
-    channel_info, video_id = get_channel(url)
+    channel_info, video_id, video_original_url = get_channel(url)
     dynamo_video_id = get_item(url)
     if not video_id == dynamo_video_id:
         put_item(url, channel_info, video_id)
+        base_url = os.environ['ApiUrl']
+        url = f'{base_url}/download?url={video_original_url}'
+        requests.get(url)
 
     return({
             'statusCode': 200,
