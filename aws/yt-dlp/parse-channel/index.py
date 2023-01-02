cde792f6d32a64d146b662466e346b2e6b6bc6fe
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-02 12:53:23 -0500

    Add cover photo

--- aws/yt-dlp/parse-channel/index.py
+++ aws/yt-dlp/parse-channel/index.py
@@ -13,11 +13,11 @@ def handler(event, context):
         # Parses SQS Input
         url = event['Records'][0]['body']
 
-    channel_info, video_id, video_original_url = get_channel_info(url)
+    channel_info, video_id, video_original_url, cover_photo = get_channel_info(url)
     dynamo_video_id = get_item(url)
 
     if not video_id == dynamo_video_id:
-        put_item(url, channel_info, video_id, video_original_url)
+        put_item(url, channel_info, video_id, video_original_url, cover_photo)
 
     return({
             'statusCode': 200,
