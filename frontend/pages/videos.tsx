b30f415977f7e7f5dd3c6fc3311d1822a4610ff3
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-03 20:09:27 -0500

    Format videos

--- frontend/pages/videos.tsx
+++ frontend/pages/videos.tsx
@@ -6,23 +6,23 @@ import { redis, scan } from '../lib/database'
 export default function videos({ videos }: any) {
   return (
     <div className='flex justify-center mt-4'>
-      <div>
+      <div className='grid grid-cols-5 gap-8 max-w-[1400px] min-w[400px] min-h[200px]'>
         {videos.map((video: any, id: Key) => (
           <div key={id} className='mt-4'>
-              <div className='hover:cursor-pointer'>
-                {video.title.S}
-                <Link href={video.original_url.S} passHref legacyBehavior>
-                  <a target='_blank' rel='noopener noreferrer'>
-                    <Image
-                      src={video.thumbnail.S}
-                      alt=''
-                      width={360}
-                      height={200}
-                    />
-                  </a>
-                </Link>
-              </div>
+            <div className='hover:cursor-pointer'>
+              <Link href={video.original_url.S} passHref legacyBehavior>
+                <a target='_blank' rel='noopener noreferrer'>
+                  <Image
+                    src={video.thumbnail.S}
+                    alt=''
+                    width={360}
+                    height={200}
+                  />
+                </a>
+              </Link>
+              <div>{video.title.S}</div>
             </div>
+          </div>
         ))}
       </div>
     </div>
