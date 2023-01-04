df5f6d4835f1e5fc98a700492a223c167023b3f0
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-03 19:53:42 -0500

    Add another url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: 'yt3.ggpht.com',
         pathname: '**'
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '**'
+      }
     ]
   }
 }
