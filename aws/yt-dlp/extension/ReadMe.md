191071a5da92ff2dfd62b5c193b431da4a670c88
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-08 20:40:23 +0000

    Add Basic Extension

---
+++ aws/yt-dlp/extension/ReadMe.md
@@ -0,0 +1,7 @@
+# Description
+Queries API to Save YouTube URL
+
+Referenced `tabs-tabs-tabs` code.
+
+## GitHub
+- https://github.com/mdn/webextensions-examples/tree/main/tabs-tabs-tabs
---
+++ aws/yt-dlp/extension/tabs.js
@@ -0,0 +1,49 @@
+// Zoom constants. Define Max, Min, increment and default values
+const ZOOM_INCREMENT = 0.2;
+const MAX_ZOOM = 5;
+const MIN_ZOOM = 0.3;
+const DEFAULT_ZOOM = 1;
+
+function getCurrentWindowTabs() {
+  return browser.tabs.query({ currentWindow: true });
+}
+
+function queryAPI(url) {
+  fetch("https://api-yt.homelabwithkevin.com/?url=" + url)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(() => {
+      console.log("Error!");
+    });
+}
+
+function notification(url) {
+  const title = browser.i18n.getMessage("notificationTitle");
+  const content = browser.i18n.getMessage("Test");
+  browser.notifications.create({
+    type: "basic",
+    title: "Saved Url",
+    message: url,
+  });
+}
+
+document.addEventListener("click", (e) => {
+  function callOnActiveTab(callback) {
+    getCurrentWindowTabs().then((tabs) => {
+      for (let tab of tabs) {
+        if (tab.active) {
+          console.log(tab.url);
+          notification(tab.url);
+          queryAPI(tab.url);
+          callback(tab, tabs);
+        }
+      }
+    });
+  }
+
+  if (e.target.id === "save-url") {
+    callOnActiveTab((tab, tabs) => { });
+  }
+  e.preventDefault();
+});
