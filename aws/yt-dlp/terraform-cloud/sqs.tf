cde6ede4a6ac70c54fd06f5743d1210369e0c1ab
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-08 13:13:00 +0000

    Update SQS timeout

--- aws/yt-dlp/terraform-cloud/sqs.tf
+++ aws/yt-dlp/terraform-cloud/sqs.tf
@@ -2,19 +2,19 @@ module "sqs-tf" {
     source = "./modules/sqs/"
     name = "sqs-tf"
     delay_seconds = "30"
-    visibility_timeout_seconds = "30"
+    visibility_timeout_seconds = "900"
 }
 
 module "sqs-videos" {
     source = "./modules/sqs/"
     name = "yt-dlp-videos-tf"
     delay_seconds = "30"
-    visibility_timeout_seconds = "30"
+    visibility_timeout_seconds = "900"
 }
 
 module "sqs-channels" {
     source = "./modules/sqs/"
     name = "yt-dlp-channels-tf"
     delay_seconds = "30"
-    visibility_timeout_seconds = "30"
+    visibility_timeout_seconds = "900"
 }
