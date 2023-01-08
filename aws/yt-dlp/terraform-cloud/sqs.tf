aab8767e17e76430d3ccc6db4711819e03932b03
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-08 18:52:55 +0000

    Update SQS to 0 delay

--- aws/yt-dlp/terraform-cloud/sqs.tf
+++ aws/yt-dlp/terraform-cloud/sqs.tf
@@ -1,20 +1,20 @@
 module "sqs-tf" {
     source = "./modules/sqs/"
     name = "sqs-tf"
-    delay_seconds = "30"
+    delay_seconds = "0"
     visibility_timeout_seconds = "900"
 }
 
 module "sqs-videos" {
     source = "./modules/sqs/"
     name = "yt-dlp-videos-tf"
-    delay_seconds = "30"
+    delay_seconds = "0"
     visibility_timeout_seconds = "900"
 }
 
 module "sqs-channels" {
     source = "./modules/sqs/"
     name = "yt-dlp-channels-tf"
-    delay_seconds = "30"
+    delay_seconds = "0"
     visibility_timeout_seconds = "900"
 }
