57a1d752d97dadf2edb27481bf5031e80a8edbc9
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-08 01:52:17 +0000

    Add video sqs queue

--- aws/yt-dlp/terraform/sqs.tf
+++ aws/yt-dlp/terraform/sqs.tf
@@ -6,3 +6,21 @@ resource "aws_sqs_queue" "sqs" {
     Name = "nextjs13-yt-dlp-Queue-tf"
   }
 }
+
+resource "aws_sqs_queue" "videos" {
+  name                          = "nextjs13-yt-dlp-Videos-tf"
+  delay_seconds                 = 90
+  visibility_timeout_seconds    = 90
+
+  tags = {
+    Name = "nextjs13-yt-dlp-Videos-tf"
+  }
+}
+
+resource "aws_sqs_queue" "videos-dlq" {
+  name = "nextjs13-yt-dlp-videos-dlq-tf"
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.videos.arn]
+  })
+}
