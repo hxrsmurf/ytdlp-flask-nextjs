56f308fafd4cd13f2d0577ad0bb791288ec8ac16
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-11 01:31:15 +0000

    Add SQS workspace

---
+++ aws/yt-dlp/terraform-cloud/sqs/sqs.tf
@@ -0,0 +1,27 @@
+module "sqs-tf" {
+    source = "../modules/sqs/"
+    name = "sqs-tf"
+    delay_seconds = "0"
+    visibility_timeout_seconds = "900"
+}
+
+module "sqs-videos" {
+    source = "../modules/sqs/"
+    name = "yt-dlp-videos-tf"
+    delay_seconds = "0"
+    visibility_timeout_seconds = "900"
+}
+
+module "sqs-channels" {
+    source = "../modules/sqs/"
+    name = "yt-dlp-channels-tf"
+    delay_seconds = "0"
+    visibility_timeout_seconds = "900"
+}
+
+module "sqs-new-video" {
+    source = "../modules/sqs/"
+    name = "yt-dlp-new-video-tf"
+    delay_seconds = "0"
+    visibility_timeout_seconds = "60"
+}
