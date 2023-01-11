25c9471906f57b179ad3d62ba5b432a999b76a03
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-11 23:04:42 +0000

    Outputs - Add default Lambda Execution Role

--- aws/yt-dlp/terraform-cloud/outputs/policy.tf
+++ aws/yt-dlp/terraform-cloud/outputs/policy.tf
@@ -12,3 +12,7 @@ data "terraform_remote_state" "policy" {
 output "policy" {
     value = data.terraform_remote_state.policy.outputs
 }
+
+output "arn-default-lambda-execution" {
+  value = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
