2bcf00ee32b4120e00dbb2c9dc6fa7906ee04142
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 15:18:14 +0000

    Update google-spaces function to use sns

--- aws/yt-dlp/terraform-cloud/functions/google-spaces/code/index.py
+++ aws/yt-dlp/terraform-cloud/functions/google-spaces/code/index.py
@@ -2,7 +2,7 @@ import logging
 from json import dumps
 import requests
 import os
-from functions.parse import parse_sqs_channel_video
+from functions.parse import parse_sqs_channel_video, parse_sns_message
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,6 +13,8 @@ def  viaRequests(url, message_headers, bot_message):
         print('Unsuccessful')
 
 def handler(event, context):
+    subject, message = parse_sns_message(event)
+
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
     space = os.environ['Space']
@@ -22,11 +24,9 @@ def handler(event, context):
     url = f'https://chat.googleapis.com/v1/spaces/{space}/messages?'\
           f'key={key}'\
           f'&token={token}'
-    
-    channel, video, title = parse_sqs_channel_video(event)
 
     bot_message = {
-        'text': f'{channel}\n{title}\n{video}'
+        'text': f'{subject}\n{message}}'
     }
 
     logging.info(bot_message)
