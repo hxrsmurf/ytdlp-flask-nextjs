a1198f1712e7695c51213865b0403190d35d09ff
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 15:42:01 +0000

    Add eventbridge output

--- aws/yt-dlp/terraform-cloud/functions/list-channels/outputs.tf
+++ aws/yt-dlp/terraform-cloud/functions/list-channels/outputs.tf
@@ -1,3 +1,7 @@
 output "list-channels" {
-    value = module.list-channels
+  value = module.list-channels
+}
+
+output "eventbridge" {
+  value = "https://us-east-1.console.aws.amazon.com/scheduler/home?region=us-east-1#schedules/default/${aws_scheduler_schedule.list-channels.name}"
 }
