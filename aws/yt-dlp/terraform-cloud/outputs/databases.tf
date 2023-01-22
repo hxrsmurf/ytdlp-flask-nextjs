1c5f1e31aeec59cb146bea921926f08793ee6ce2
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 14:50:28 +0000

    Add database URLs

--- aws/yt-dlp/terraform-cloud/outputs/databases.tf
+++ aws/yt-dlp/terraform-cloud/outputs/databases.tf
@@ -10,5 +10,12 @@ data "terraform_remote_state" "databases" {
 }
 
 output "databases" {
-    value = data.terraform_remote_state.databases.outputs
+  value = data.terraform_remote_state.databases.outputs
+}
+
+output "database-urls" {
+  value = {
+    channels = "https://us-east-1.console.aws.amazon.com/dynamodbv2/home?region=us-east-1#table?name=${data.terraform_remote_state.databases.outputs.channels.name}"
+    videos   = "https://us-east-1.console.aws.amazon.com/dynamodbv2/home?region=us-east-1#table?name=${data.terraform_remote_state.databases.outputs.videos.name}"
+  }
 }
