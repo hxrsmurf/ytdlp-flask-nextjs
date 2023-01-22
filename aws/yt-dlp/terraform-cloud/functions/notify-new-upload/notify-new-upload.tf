711024850d05c23695bf1b1dcef9ebcdc93a1c2d
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 15:24:52 +0000

    Revert spacing for notify-new-upload

--- aws/yt-dlp/terraform-cloud/functions/notify-new-upload/notify-new-upload.tf
+++ aws/yt-dlp/terraform-cloud/functions/notify-new-upload/notify-new-upload.tf
@@ -10,23 +10,22 @@ data "terraform_remote_state" "outputs" {
 }
 
 module "notify-new-upload" {
-  source = "../../modules/lambda"
-  name   = "notify-new-upload"
-  layers = [
-    data.terraform_remote_state.outputs.outputs.layers.yt-dlp
-  ]
-  memory  = "256"
-  timeout = "30"
-  environment = {
-    ses_arn = data.terraform_remote_state.outputs.outputs.ses.arn
-    email   = data.terraform_remote_state.outputs.outputs.ses.email
-    sns_arn = data.terraform_remote_state.outputs.outputs.sns.sns.arn
-  }
-  policy-arn = [
-    "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole",
-    data.terraform_remote_state.outputs.outputs.arn-default-lambda-execution,
-    data.terraform_remote_state.outputs.outputs.policy.ses
-  ]
+    source = "../../modules/lambda"
+    name = "notify-new-upload"
+    layers = [
+        data.terraform_remote_state.outputs.outputs.layers.yt-dlp
+    ]
+    memory = "256"
+    timeout = "30"
+    environment = {
+        ses_arn = data.terraform_remote_state.outputs.outputs.ses.arn
+        email = data.terraform_remote_state.outputs.outputs.ses.email
+    }
+    policy-arn = [
+        data.terraform_remote_state.outputs.outputs.arn-default-lambda-execution,
+        data.terraform_remote_state.outputs.outputs.policy.ses,
+        "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+    ]
 }
 
 resource "aws_lambda_event_source_mapping" "sqs" {
