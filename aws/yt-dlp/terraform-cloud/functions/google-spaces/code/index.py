18c42ebcb961b5665df3e7696bc6639db7449c57
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 09:12:01 +0000

    Add index.py

---
+++ aws/yt-dlp/terraform-cloud/functions/google-spaces/code/index.py
@@ -0,0 +1,31 @@
+import logging
+from json import dumps
+import requests
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def  viaRequests(url, message_headers, bot_message):
+    response = requests.post(url=url, headers=message_headers, data=dumps(bot_message))
+    if not response.status_code == 200:
+        print('Unsuccessful')
+
+def handler(event, context):
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    space = os.environ['Space']
+    key = os.environ['Key']
+    token = os.environ['Token']
+
+    url = f'https://chat.googleapis.com/v1/spaces/{space}/messages?'\
+          f'key={key}'\
+          f'&token={token}'
+    
+    channel, video, title = parse_sqs_channel_video(event)
+
+    bot_message = {
+        'text': f'{channel}\n{title}\n{video}'
+    }
+
+    viaRequests(url, message_headers, bot_message)
