5bf21268227b3e2a73718f3fdb5ae13034c64fff
Kevin Murphy <kevin@hxrsmurf.info>
2023-05-21 07:34:13 -0400

    Add yt_dlp oppts

--- bulk-downloader/python.py
+++ bulk-downloader/python.py
@@ -1,6 +1,18 @@
 import argparse
+import yt_dlp
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--channel", "-c")
 args = parser.parse_args()
-print('Hello World')
-print(args.channel)
+
+ytdl_opts = {
+    'outtmpl' : '%(uploader)s/%(id)s.%(ext)s',
+    'windowsfilenames': True,
+    'playlistend' : 1,
+    'ignoreerrors' : True,
+    'format': 'bestvideo*+bestaudio/best',
+    'merge_output_format': 'mp4'
+}
+
+with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
+    ydl.extract_info(args.channel, download=True)
