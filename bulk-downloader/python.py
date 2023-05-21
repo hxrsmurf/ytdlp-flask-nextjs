9faed0649d5bd61e88c184cee7584b5d7c6c224d
Kevin Murphy <kevin@hxrsmurf.info>
2023-05-21 08:14:12 -0400

    Add redis support

--- bulk-downloader/python.py
+++ bulk-downloader/python.py
@@ -1,5 +1,7 @@
 import argparse
 import yt_dlp
+import time
+import redis
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--channel", "-c")
@@ -14,5 +16,26 @@ ytdl_opts = {
     'merge_output_format': 'mp4'
 }
 
-with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
-    ydl.extract_info(args.channel, download=True)
+r = redis.Redis(
+    host="redis",
+    port=6379,
+    db=0,
+    decode_responses=True
+)
+
+pub = r.pubsub()
+pub.subscribe('queue')
+
+while True:
+    message = pub.get_message()
+
+    if message:
+        # Have to do try/except to ensure python stays alive
+        try:
+            message_json = message['data']
+            print(message_json)
+
+            with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
+                ydl.extract_info(message_json, download=True)
+        except:
+            print('No messages')
